663a701af677b1e1f2e4ea55d8437e053c557710
mrhomayouni <83701100+mrhomayouni@users.noreply.github.com>
2023-01-08 22:23:23 +0330

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,11 +1,36 @@
 <?php
 
-// utility sql:
-// for get information fo a table
-// "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'song';"
-//  show columns from ads; 
+header('Content-type: image/jpeg');
+$time = time();
+$image = imagecreate(200, 100);
+$color_line = imagecolorallocate($image, rand(0, 255), rand(0, 255), rand(0, 255));
+$color_pixle = imagecolorallocate($image, rand(0, 255), rand(0, 255), rand(0, 255));
+$color_background = imagecolorallocate($image, 0, 0, 0);
+$text_color = imagecolorallocate($image, 255, 255, 255);
+imagefilledrectangle($image, 0, 0, 200, 100, $color_background);
+
+for ($i = 1; $i <= 10; $i++) {
+    imageline($image, rand(0, 100), rand(0, 100), 200, rand(0, 100), $color_line);
+}
+for ($i = 1; $i <= 1000; $i++) {
+    imagesetpixel($image, rand(0, 200), rand(0, 100), $color_pixle);
+}
+
+$letters = "0123456789";
+$len = strlen($letters);
+$word = "";
+$font = "public/app/fonts/Fairytail-p7P41.ttf";
+for ($i = 1; $i < 4; $i++) {
+    $letter = $letters[rand(0, $len - 1)];
+    imagettftext($image, 60, rand(20, 40), 5 + ($i * 45), 90, $text_color, $font, $letter);
+    $word = $word . $letter;
+    $_SESSION["captcha"] = $word;
+}
+$color_line = imagecolorallocate($image, rand(0, 255), rand(0, 255), rand(0, 255));
+for ($i = 1; $i <= 10; $i++) {
+    imageline($image, rand(0, 100), rand(0, 100), 200, rand(0, 100), $color_line);
+}
+imagepng($image);
 
 
 
-
-  ?>
